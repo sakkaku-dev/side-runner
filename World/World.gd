47cce33c12bd3668030c4c4799cd952166d5cb73
myin <minyin142p@gmail.com>
2020-09-12 09:15:42 +0200

    Add highscore

--- World/World.gd
+++ World/World.gd
@@ -8,6 +8,24 @@ onready var player := $Player
 onready var bgm := $BGM
 onready var player_died_sound := $PlayerDied
 onready var start_block := $StartBlock/CollisionShape2D
+onready var score_label := $CanvasLayer/HBoxContainer/MarginContainer/Score
+
+var high_score = -1 setget set_high_score
+
+func set_high_score(score: int) -> void:
+	if high_score != score:
+		high_score = score
+		print(high_score)
+		score_label.text = "Score: " + str(high_score)
+
+func _process(delta: float) -> void:
+	if player == null:
+		return
+
+	if player.global_position.x <= 1000:
+		self.high_score = 0
+	else:
+		self.high_score = floor(player.global_position.x / 1000)
 
 func set_left_limit(pos_x: int) -> void:
 	camera.limit_left = pos_x
@@ -21,6 +39,7 @@ func start_game():
 	camera.limit_right = 10000000
 	start_block.disabled = true
 	bgm.play()
+	self.high_score = 0
 	
 func restart_game():
 	if get_tree().change_scene("res://World/World.tscn"):
